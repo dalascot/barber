8eb0bfc2383f8f64c1c0bf6c16c9899c7c0b4929
dalascot <159759273+dalascot@users.noreply.github.com>
2024-06-14 18:59:03 +0100

    Update AddressInputs.js

--- src/components/layout/AddressInputs.js
+++ src/components/layout/AddressInputs.js
@@ -7,34 +7,34 @@ export default function AddressInputs({addressProps,setAddressProp,disabled=fals
         disabled={disabled}
         type="tel" placeholder="Phone number"
         value={phone || ''} onChange={ev => setAddressProp('phone', ev.target.value)} />
-      <label>Street address</label>
+      <label>Quartier</label>
       <input
         disabled={disabled}
-        type="text" placeholder="Street address"
+        type="text" placeholder="Quartier"
         value={streetAddress || ''} onChange={ev => setAddressProp('streetAddress', ev.target.value)}
       />
       <div className="grid grid-cols-2 gap-2">
         <div>
-          <label>Postal code</label>
+          <label>Numero</label>
           <input
             disabled={disabled}
-            type="text" placeholder="Postal code"
+            type="text" placeholder="Numero 2"
             value={postalCode || ''} onChange={ev => setAddressProp('postalCode', ev.target.value)}
           />
         </div>
         <div>
-          <label>City</label>
+          <label> Sous Quartier</label>
           <input
             disabled={disabled}
-            type="text" placeholder="City"
+            type="text" placeholder="Sous Quartier"
             value={city || ''} onChange={ev => setAddressProp('city', ev.target.value)}
           />
         </div>
       </div>
-      <label>Country</label>
+      <label>Ville/label>
       <input
         disabled={disabled}
-        type="text" placeholder="Country"
+        type="text" placeholder="ville"
         value={country || ''} onChange={ev => setAddressProp('country', ev.target.value)}
       />
     </>
