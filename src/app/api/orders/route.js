a0fdc9423882b27638c30b1c01eecf63eb685920
dalascot <159759273+dalascot@users.noreply.github.com>
2024-06-14 19:40:30 +0100

    Update route.js

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -6,23 +6,22 @@ import {getServerSession} from "next-auth";
 export async function GET(req) {
   mongoose.connect(process.env.MONGO_URL);
 
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  const admin = await isAdmin();
-
-  const url = new URL(req.url);
+ const session = await getServerSession(authOptions);
+    const userEmail = session?.user?.email;
+    const admin = await User.findOne({email:userEmail})
+    const url = new URL(req.url);
+    if (userEmail) {
+        return Response.json( await Order.find({userEmail}) );
+      }
+      
   const _id = url.searchParams.get('_id');
   if (_id) {
     return Response.json( await Order.findById(_id) );
   }
-
-
-  if (admin) {
-    return Response.json( await Order.find() );
-  }
-
-  if (userEmail) {
-    return Response.json( await Order.find({userEmail}) );
-  }
-
-}
+      if(admin){
+        return Response.json(await Order.find())
+      }
+      else{return ('error')}
+  
+   
+   }
