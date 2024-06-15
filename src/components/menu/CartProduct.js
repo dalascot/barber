1de750afe078712a5fbd74c816e22b8488753c00
dalascot <159759273+dalascot@users.noreply.github.com>
2024-06-15 18:30:28 +0100

    Update CartProduct.js

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -6,7 +6,7 @@ export default function CartProduct({product,onRemove}) {
   return (
     <div className="flex items-center gap-4 border-b py-4">
       <div className="w-24">
-        <Image width={240} height={240} src={product.image} alt={''} />
+        <Image width={240} height={500} src={product.image} alt={''} />
       </div>
       <div className="grow">
         <h3 className="font-semibold">
@@ -32,7 +32,7 @@ export default function CartProduct({product,onRemove}) {
         <div className="ml-2">
           <button
             type="button"
-            onClick={() => onRemove(index)}
+            onClick={onRemove(index)}
             className="p-2">
             <Trash />
           </button>
